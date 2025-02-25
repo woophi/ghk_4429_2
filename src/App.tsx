b88f4f61a086b43adc3acf4f9a56e7e55ef3f1c8
Konstantin Mikheev <mik.kon6610@gmail.com>
2025-02-25 15:27:12 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ export const App = () => {
     if (withThx) {
       window.gtag('event', 'activate_gold_4429_var2');
     } else {
-      window.gtag('event', 'activate_fin_4429_var2');
+      window.gtag('event', 'no_interest_4429_var2');
     }
     setLoading(true);
 
